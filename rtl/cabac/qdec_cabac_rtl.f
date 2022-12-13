586b3bc70417ef6cbdadd573f5fa9a1f007047ee
yuoyuoyan <yuoyuoyan@gmail.com>
2022-12-13 11:31:07 +0800

    Some fix for vcs sim

--- rtl/cabac/qdec_cabac_rtl.f
+++ rtl/cabac/qdec_cabac_rtl.f
@@ -1,5 +1,6 @@
 +incdir+../../rtl/cabac/
 +incdir+../../rtl/cmn/
+../../rtl/cmn/axi_pkg.sv
 ../../rtl/cabac/qdec_cabac_package.sv
 ../../rtl/cabac/qdec_cabac.sv
 ../../rtl/cabac/qdec_cabac_register.sv
--- sim/cabac/cabac.tb.f
+++ sim/cabac/cabac.tb.f
@@ -1,3 +1,3 @@
+-f ../../rtl/cabac/qdec_cabac_rtl.f
 +incdir+../../tb/cmn/
 ../../tb/tb_cabac.sv
--f ../../rtl/cabac/qdec_cabac_rtl.f
